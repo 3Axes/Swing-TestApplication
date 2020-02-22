ef131f2a9c3a4b50ed111375a7bcaabfdd8d65a2
3Axes <divyank.jain1@gmail.com>
2020-02-21 19:11:33 -0500

    feb 21

---
+++ src/feb/test1.java
@@ -0,0 +1,23 @@
+package feb;
+
+import javax.swing.*;
+
+public class test1 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    public static void createAndShowGUI() {
+        JFrame frame = new JFrame("Title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Text");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
