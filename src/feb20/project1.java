f6c5f7f571a3d0bb55f9f9c9b8d6a4809115f14b
3Axes <divyank.jain1@gmail.com>
2020-02-27 20:26:10 -0500

    new things

---
+++ src/feb20/project1.java
@@ -0,0 +1,18 @@
+package feb20;
+
+import javax.swing.*;
+
+public class project1 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                gui();
+            }
+        });
+    }
+
+    public static void gui() {
+
+    }
+}
